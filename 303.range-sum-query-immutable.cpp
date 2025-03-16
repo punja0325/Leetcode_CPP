aff8bca15a5b0c8bf8692c6742ce24f9a22fa411
Punja <punja0325@gmail.com>
2025-03-16 14:37:38 +0800

    new

---
+++ 303.range-sum-query-immutable.cpp
@@ -0,0 +1,87 @@
+/*
+ * @lc app=leetcode id=303 lang=cpp
+ *
+ * [303] Range Sum Query - Immutable
+ *
+ * https://leetcode.com/problems/range-sum-query-immutable/description/
+ *
+ * algorithms
+ * Easy (63.27%)
+ * Likes:    3426
+ * Dislikes: 1951
+ * Total Accepted:    669.2K
+ * Total Submissions: 993.2K
+ * Testcase Example:  '["NumArray","sumRange","sumRange","sumRange"]\n' +
+  '[[[-2,0,3,-5,2,-1]],[0,2],[2,5],[0,5]]'
+ *
+ * Given an integer array nums, handle multiple queries of the following
+ * type:
+ *
+ *
+ * Calculate the sum of the elements of nums between indices left and right
+ * inclusive where left <= right.
+ *
+ *
+ * Implement the NumArray class:
+ *
+ *
+ * NumArray(int[] nums) Initializes the object with the integer array nums.
+ * int sumRange(int left, int right) Returns the sum of the elements of nums
+ * between indices left and right inclusive (i.e. nums[left] + nums[left + 1] +
+ * ... + nums[right]).
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input
+ * ["NumArray", "sumRange", "sumRange", "sumRange"]
+ * [[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+ * Output
+ * [null, 1, -1, -3]
+ *
+ * Explanation
+ * NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+ * numArray.sumRange(0, 2); // return (-2) + 0 + 3 = 1
+ * numArray.sumRange(2, 5); // return 3 + (-5) + 2 + (-1) = -1
+ * numArray.sumRange(0, 5); // return (-2) + 0 + 3 + (-5) + 2 + (-1) = -3
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 1 <= nums.length <= 10^4
+ * -10^5 <= nums[i] <= 10^5
+ * 0 <= left <= right < nums.length
+ * At most 10^4 calls will be made to sumRange.
+ *
+ *
+ */
+
+// @lc code=start
+class NumArray {
+private:
+    vector<int> prefix;
+public:
+    NumArray(vector<int>& nums) {
+        prefix = vector<int>(nums.size() + 1, 0);
+        prefix[0] = nums[0];
+        for(int i = 0; i < nums.size(); i++) {
+            prefix[i + 1] = prefix[i] + nums[i];
+        }
+    }
+
+    int sumRange(int left, int right) {
+        return prefix[right + 1] - prefix[left];
+    }
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * int param_1 = obj->sumRange(left,right);
+ */
+// @lc code=end
+
