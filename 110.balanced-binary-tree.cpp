509d4a78aed719faf9efdd63d5d23f3f7e81ddf7
Chia-Ching <punja0325@gmail.com>
2025-03-27 23:40:31 +0800

    new

---
+++ 110.balanced-binary-tree.cpp
@@ -0,0 +1,79 @@
+/*
+ * @lc app=leetcode id=110 lang=cpp
+ *
+ * [110] Balanced Binary Tree
+ *
+ * https://leetcode.com/problems/balanced-binary-tree/description/
+ *
+ * algorithms
+ * Easy (54.32%)
+ * Likes:    11248
+ * Dislikes: 757
+ * Total Accepted:    1.9M
+ * Total Submissions: 3.4M
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * Given a binary tree, determine if it is height-balanced.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: root = [3,9,20,null,null,15,7]
+ * Output: true
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: root = [1,2,2,3,3,null,null,4,4]
+ * Output: false
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: root = []
+ * Output: true
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of nodes in the tree is in the range [0, 5000].
+ * -10^4 <= Node.val <= 10^4
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        return checkHeight(root) != -1;
+    }
+    int checkHeight(TreeNode* root) {
+        if (root == NULL) return 0;
+        int leftHeight = checkHeight(root->left);
+        if (leftHeight == -1) return -1;
+        int rightHeight = checkHeight(root->right);
+        if (rightHeight == -1) return -1;
+
+        if (abs(leftHeight - rightHeight) > 1) return -1;
+        return max(leftHeight, rightHeight) + 1;
+    }
+};
+// @lc code=end
+
