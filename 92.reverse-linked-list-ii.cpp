7b9537ed2978b6345838be92bc00b22ef40816b3
Punja <punja0325@gmail.com>
2025-04-05 21:11:17 +0800

    new

---
+++ 92.reverse-linked-list-ii.cpp
@@ -0,0 +1,84 @@
+/*
+ * @lc app=leetcode id=92 lang=cpp
+ *
+ * [92] Reverse Linked List II
+ *
+ * https://leetcode.com/problems/reverse-linked-list-ii/description/
+ *
+ * algorithms
+ * Medium (47.66%)
+ * Likes:    12148
+ * Dislikes: 694
+ * Total Accepted:    1.1M
+ * Total Submissions: 2.1M
+ * Testcase Example:  '[1,2,3,4,5]\n2\n4'
+ *
+ * Given the head of a singly linked list and two integers left and right where
+ * left <= right, reverse the nodes of the list from position left to position
+ * right, and return the reversed list.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: head = [1,2,3,4,5], left = 2, right = 4
+ * Output: [1,4,3,2,5]
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: head = [5], left = 1, right = 1
+ * Output: [5]
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * The number of nodes in the list is n.
+ * 1 <= n <= 500
+ * -500 <= Node.val <= 500
+ * 1 <= left <= right <= n
+ *
+ *
+ *
+ * Follow up: Could you do it in one pass?
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        ListNode dummy(0, head);
+        ListNode* prev = &dummy;
+        ListNode* curr = head;
+        for(int i = 0; i < left - 1; i++) {
+            prev = curr;
+            curr = curr->next;
+        }
+        ListNode* preList = prev;
+        prev = NULL;
+        for(int i = 0; i < right - left + 1; i++) {
+            ListNode* tempNode = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = tempNode;
+        }
+        preList->next->next = curr;
+        preList->next = prev;
+        return dummy.next;
+    }
+};
+// @lc code=end
+
