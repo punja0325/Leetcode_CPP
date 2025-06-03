91c9baf8a04e1abbbfa84439faf6e48a52ff2447
Punja <punja0325@gmail.com>
2025-06-04 00:19:52 +0800

    new

--- 74.search-a-2-d-matrix.cpp
+++ 74.search-a-2-d-matrix.cpp
@@ -15,43 +15,43 @@
  *
  * You are given an m x n integer matrix matrix with the following two
  * properties:
- * 
- * 
+ *
+ *
  * Each row is sorted in non-decreasing order.
  * The first integer of each row is greater than the last integer of the
  * previous row.
- * 
- * 
+ *
+ *
  * Given an integer target, return true if target is in matrix or false
  * otherwise.
- * 
+ *
  * You must write a solution in O(log(m * n)) time complexity.
- * 
- * 
+ *
+ *
  * Example 1:
- * 
- * 
+ *
+ *
  * Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
  * Output: true
- * 
- * 
+ *
+ *
  * Example 2:
- * 
- * 
+ *
+ *
  * Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
  * Output: false
- * 
- * 
- * 
+ *
+ *
+ *
  * Constraints:
- * 
- * 
+ *
+ *
  * m == matrix.length
  * n == matrix[i].length
  * 1 <= m, n <= 100
  * -10^4 <= matrix[i][j], target <= 10^4
- * 
- * 
+ *
+ *
  */
 
 // @lc code=start
@@ -67,6 +67,7 @@ public:
             else up = mid + 1;
         }
         int row = down;
+        if (row < 0) return false;
         int left = 0, right = n - 1;
         while(left <= right) {
             int mid = left + (right - left) / 2;
@@ -75,8 +76,8 @@ public:
             else left = mid + 1;
         }
         return false;
-        
     }
 };
+
 // @lc code=end
 
